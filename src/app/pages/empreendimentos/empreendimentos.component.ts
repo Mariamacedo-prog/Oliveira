0c9930193eceaeb9ca95a75a14813d3e73ff4870
MariaMacedo-Prog <mariamacedo2xx@gmail.com>
2022-10-16 18:32:17 -0300

    empreendimento

--- src/app/pages/empreendimentos/empreendimentos.component.ts
+++ src/app/pages/empreendimentos/empreendimentos.component.ts
@@ -8,11 +8,14 @@ import { DataService } from 'src/app/data.service';
   styleUrls: ['./empreendimentos.component.scss']
 })
 export class EmpreendimentosComponent implements OnInit {
+  empreendimentoFilter: any = [];
+  empreendimentos: any = [];
   pageInfo: any = {};
   images: any = [];
   whatsLink: string = '';
   endereco: any = {};
   image64: any = {};
+  activeOption = 'TODOS';
   constructor(private router: Router, private dataService: DataService) { }
 
   async ngOnInit() {
@@ -23,7 +26,24 @@ export class EmpreendimentosComponent implements OnInit {
       this.whatsLink = data.oliveiraInfo.whatsLink;
       this.endereco = data.oliveiraInfo.endereco;
       this.image64 = data.image64;
+
+      this.empreendimentoFilter = data.pageEmpreendimentos.arrayEmpreendimentos;
+      this.empreendimentos = data.pageEmpreendimentos.arrayEmpreendimentos;
     })
+  }
+
+  filterEmpreendimento(status: string){
+    this.activeOption = status;
+
+    if(status === 'TODOS'){
+      this.empreendimentoFilter = this.empreendimentos;
+    }else{
+      let result = this.empreendimentos.filter((item: any) => item.status === status);
+      console.log(result);
+
+      this.empreendimentoFilter = result;
+    }
+
   }
 
 }
